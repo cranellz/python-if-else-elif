beed0e8c4ff9ae1b42e1d5b04026ecfe6d9ee8db
cranellz <cranellswing.230@gmail.com>
2026-03-06 10:14:04 -0300

    Add files via upload

---
+++ PythonNotas.py
@@ -0,0 +1,9 @@
+a = float(input("digite a nota: "))
+if a >= 8:
+    print("aprovado com nota A")
+elif a >= 7:
+    print("aprovado com nota B")
+elif a >= 5:
+    print("aprovado com nota C")
+else:
+    print("reprovado")
